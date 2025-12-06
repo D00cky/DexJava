006d19e4685376e07322104b87e71bb3dc91a58c
d00 <83293500+D00cky@users.noreply.github.com>
2025-12-06 19:24:44 -0300

    Succeed to made the first GET requisition

--- src/main/java/com/pokeApi/DexJava/DexJavaApplication.java
+++ src/main/java/com/pokeApi/DexJava/DexJavaApplication.java
@@ -2,6 +2,7 @@ package com.pokeApi.DexJava;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
---
+++ src/main/java/com/pokeApi/DexJava/model/DexJavaPokemonModel.java
@@ -0,0 +1,5 @@
+package com.pokeApi.DexJava.model;
+
+
+public class DexJavaPokemonModel {
+}
