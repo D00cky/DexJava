09b87434335e4bbd4e015f667979c2777215772c
d00 <83293500+D00cky@users.noreply.github.com>
2025-12-06 16:22:42 -0300

    Added Files to the PokeDexProject

---
+++ src/main/java/com/pokeApi/DexJava/DexJavaApplication.java
@@ -0,0 +1,13 @@
+package com.pokeApi.DexJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class DexJavaApplication {
+
+	public static void main(String[] args) {
+        SpringApplication.run(DexJavaApplication.class, args);
+    }
+}
---
+++ src/test/java/com/pokeApi/DexJava/DexJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokeApi.DexJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DexJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
